1eed88c3c9a9a9889ae4e523e1c25104f629072b
Mwuschnig <m.wuschnig@gmx.at>
2022-03-09 20:25:49 +0100

    update time.py opentheses

--- time.py
+++ time.py
@@ -4,10 +4,13 @@ import time
 date = str(int(time.time()))
 
 
-input_file = open('./data/pages/research/opentheses.md' ,'r+')
-
+input_file = open('./data/pages/research/opentheses.md' ,'r')
 all_lines = input_file.readlines()
-all_lines[10] = "{% assign date = "+date+" %}"
-input_file.writelines(all_lines)
 input_file.close()
+
+outputfile = open('./data/pages/research/opentheses.md' ,'w')
+all_lines[10] = "{% assign date = "+date+" %}\n"
+outputfile.writelines(all_lines)
+outputfile.close()
+
 
