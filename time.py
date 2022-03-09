ef6b0f0d0fbcbbe856be99abaaf3501b119c2077
Mwuschnig <Mwuschnig@users.noreply.github.com>
2022-03-09 19:13:10 +0000

    deploy: 755be91f4d0ba1598d4d361b8a56eebb6822e3ef

--- time.py
+++ time.py
@@ -6,19 +6,8 @@ date = str(int(time.time()))
 
 input_file = open('./data/pages/research/opentheses.md' ,'r+')
 
-
-
 all_lines = input_file.readlines()
-
-outputlines = []
-
-for line in all_lines:
-	if("{% assign date =" in line):
-		outputlines.append("{% assign date = "+date+" %}" )
-	else:
-		outputlines.append(line)
-
-
-input_file.writelines(outputlines)
-
+all_lines[10] = "{% assign date = "+date+" %}"
+input_file.writelines(all_lines)
 input_file.close()
+
