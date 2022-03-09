9cde0837c91811b66ec6160899d3341eb41f0325
Mwuschnig <m.wuschnig@gmx.at>
2022-03-09 12:07:31 +0100

    add python3 timefunction

---
+++ time.py
@@ -0,0 +1,24 @@
+import time
+
+
+date = str(int(time.time()))
+
+
+input_file = open('/data/research/opentheses.md' ,'r+')
+
+
+
+all_lines = input_file.readlines()
+
+outputlines = []
+
+for line in all_lines:
+	if("{% assign date =" in line):
+		outputlines.append("{% assign date = "+date+" %}" )
+	else:
+		outputlines.append(line)
+
+
+input_file.writelines(outputlines)
+
+input_file.close()
